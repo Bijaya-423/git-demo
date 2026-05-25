a8b32950786566970e6b7f76db6f1ebea0febd0f
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-25 21:42:34 +0530

    added

---
+++ day-16/arguments.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#argument is the keyword written after filename
+
+<<comment
+
+this
+is a
+multiline  comment
+
+usage of argument ./arguments.sh bijaya pinkun
+
+comment
+
+echo "The 0th argument is $0"
+echo "The first argument is $1"
+echo "The second argument is $2"
+echo "The third argument is $3"
