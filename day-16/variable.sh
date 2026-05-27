7af111584b5df2a75b784b22b28dfd53afb94032
Bijaya-423 <bijayakumarjena423@gmail.com>
2026-05-27 19:52:56 +0530

    added

---
+++ day-16/variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+NAME="Bijaya kumar jena"
+ROLE='DEVOPS ENGINEER'
+
+# echo "Hello, i am $NAME and i am a $ROLE" -> it print constant
+
+echo 'Hello, i am $NAME and i am a $ROLE' -> it print only variable 
